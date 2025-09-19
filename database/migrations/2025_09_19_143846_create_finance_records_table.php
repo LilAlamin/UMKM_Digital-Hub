0338ffe7986f30e8abf460724ba8fd1d77675cd6
LilAlamin <ronaldramadhani15@gmail.com>
2025-09-19 22:01:45 +0700

    Feat : Add Migration Table

---
+++ database/migrations/2025_09_19_143846_create_finance_records_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('finance_records', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); // UMKM
+        $table->enum('type', ['income', 'expense']); // pemasukan/pengeluaran
+        $table->decimal('amount', 12, 2);
+        $table->string('category')->nullable(); // misal: penjualan, bahan baku
+        $table->text('note')->nullable();
+        $table->date('record_date')->default(now());
+        $table->timestamps();
+    });
+}
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('finance_records');
+    }
+};
